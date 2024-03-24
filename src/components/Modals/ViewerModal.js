82155369353db4e8b2524ee872c893b810ff6518
konstantinAtMaestroTech <konstantin@maestro-tech.com>
2024-03-24 19:59:58 +0100

    ip update

--- src/components/Modals/ViewerModal.js
+++ src/components/Modals/ViewerModal.js
@@ -6,7 +6,7 @@ const Modal = ({ handleClose, show, urn, uniqueID }) => {
 
     useEffect(() => {
 
-        const url = 'http://13.60.57.117:8080/assemblyID';
+        const url = 'https://supplier-web-app.maestrotest.info/assemblyID';
 
         const fetchData = async () => {
             try{
@@ -24,7 +24,7 @@ const Modal = ({ handleClose, show, urn, uniqueID }) => {
     }, [uniqueID]);
     
     const showHideClassName = show ? "modal display-block" : "modal display-none";
-    const viewerUrl = 'http://13.60.57.117:8080/#' + urn;
+    const viewerUrl = 'https://supplier-web-app.maestrotest.info/#' + urn;
 
     return (
     <div className={showHideClassName}>
